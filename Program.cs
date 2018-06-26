6c5dc030e85e4aab08365441be849aa758dc95f3
Fang0331 <494681517@qq.com>
2018-06-26 11:40:13 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Mongotest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            connect mydb = new connect();
+            mydb.Load();
+            IAsyncCursor<BsonDocument> mycursor= connect._database.ListCollections();
+            IMongoCollection<BsonDocument> mycollection=connect._database.GetCollection<BsonDocument> ("newscollection");
+            IFindFluent<BsonDocument,BsonProjection> myresult =mycollection.Find<BsonDocument>()
+            
+           // Console.Write(mycursor);
+            Console.Read();
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ connect.cs
@@ -0,0 +1,35 @@
+﻿
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mongotest
+{
+    class connect
+    {
+
+        private const string connUrl = "mongodb://127.0.0.1:27017";
+
+        private const string dbName = "biztest";
+
+        public static IMongoClient _client;
+        public static IMongoDatabase _database;
+        
+          public  void Load()
+        {
+
+            var url = new MongoUrl(connUrl);
+            var settings = MongoClientSettings.FromUrl(url);
+
+            //创建数据连接
+            _client = new MongoClient();
+            _database = _client.GetDatabase(dbName);
+            Console.WriteLine("biztest connected");
+
+        }
+        public void createdatabase() { }
+    }
+}
